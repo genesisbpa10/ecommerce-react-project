442e62abd6f2909b163b2fd140b0537da60cbee4
genesisbpa10 <genesisbetaly@gmail.com>
2022-07-14 00:17:31 -0500

    item-list-container added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
+  let greetingsText="Hola, estamos en proceso de construccion... ¡Vuelva pronto!"
+
   return (
     <>
       <NavBar />
+      <ItemListContainer greetings={greetingsText} />
     </>
   );
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+  return (
+    <div>{props.greetings}</div>
+  )
+}
+
+export default ItemListContainer;
