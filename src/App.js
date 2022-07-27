66973dc951dc3e82a9c0c49f3695754aec678e1c
genesisbpa10 <genesisbetaly@gmail.com>
2022-07-27 00:26:46 -0500

    new structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel  from "./components/Carousel";
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Carousel/>
       <ItemListContainer greetings="Hola, estamos trabajando en el ItemList y el Item..." />
     </>
   );
--- src/mock/FakeApi.js
+++ src/mock/FakeApi.js
@@ -1,68 +1,25 @@
 const products = [
   {
-    price:"$4000",
-    id: 3,
-    title: "Snake",
-    url: "/img/snake.jpg",
-    stock: "3",
-  },
-  {
-    price:"3000",
-    id: 1,
-    url: "/img/sheine-grey.jpg",
-    title: "Sheine Grey",
-    stock: "6",
-  },
-  {
-    price:"$2000",
-    id: 9,
-    title: "Black",
-    url: "/img/black.jpg",
-    stock: "12",
-  },
-  {
-    price:"$2800",
-    id: 6,
-    title: "Mome Black 2",
-    url: "/img/mome-black-2.jpg",
-    stock: "10",
-  },
-  {
-    price:"$4000",
-    id: 7,
-    title: "Gold",
-    url: "/img/gold.jpg",
-    stock: "2",
-  },
-  {
-    price:"$3000",
-    id: 4,
-    title: "Snake Grey ",
-    url: "/img/snake-grey.jpg",
-    stock: "8",
-  },
-  {
-    price:"$2600",
-    id: 5,
-    title: "Mome Black ",
-    url: "/img/mome-black.jpg",
-    stock: "9",
-  },
-  {
-    price:"$1900",
-    id: 8,
-    title: "Croco Red",
-    url: "/img/croco-red.jpg",
-    stock: "5",
-  },
-  {
-    price:"$2500",
-    id: 2,
-    title: "Sheine Grey 2",
-    url: "/img/sheine-grey-2.jpg",
-    stock: "4",
-  },
-];
+    id:"0-1",
+    sku:"",
+    name:"Mome",
+    price:"",
+    descriptions:"",
+    weight:"",
+    height:"",
+    principalImg:"",
+    secondaryImg:"",
+    thirdImg:"",
+    category:"",
+    stock:"",
+    thumbnail:"", 
+    type:"" //Fija o intercambiable.
+  }
+]
+
+
+
+
 
 export const data = new Promise((resolve, reject) => {
   let condition = true;
@@ -74,3 +31,40 @@ export const data = new Promise((resolve, reject) => {
     }
   }, 2000);
 });
+
+
+// export const newData = (() => fetch("https://jsonplaceholder.typicode.com/todos/1"),{
+
+//   method:"POST",
+//     body: JSON.stringify({
+//     title:"FOO",
+//     userId:1,    
+//   })
+// })
+
+// export const deleteDta = (dataId) => fetch(`https://jsonplaceholder.typicode.com/posts/${dataId}`,{
+//   method:"DELETE",
+// })
+
+// //Desafío de clase
+// //Usar el use effect para entrar en el ciclo  o en la etapa de monatje
+// const [pokemons, setPokemons] = useState([]);
+// useEffect(() => {
+//   console.log('Ya se montó el componente');
+//   axios(`${API_URL}pokemon?limit=10`)
+//   .then((response) => {
+//   console.log(response.data)
+//   setPokemons(response.data.results);
+// })
+// .catch((err) => console.log(err));
+// }, []);
+
+// return (
+//   <div>
+//     <ul>
+//       {pokemons.map((pokemon) => (
+//         <li key={pokemon.name}>{pokemon.name} </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
