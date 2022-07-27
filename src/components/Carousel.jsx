00962b38e1c02ec721803f16b028c6886245e043
genesisbpa10 <genesisbetaly@gmail.com>
2022-07-27 00:20:50 -0500

    products imgs and structure added

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { UncontrolledCarousel } from "reactstrap";
+
+const Carousel = () => {
+  const items = [
+    {
+      altText: "Slide 1",
+      caption: "Slide 1",
+      key: 1,
+      src: "/img/img-carousel-1.png",
+    },
+    {
+      altText: "Slide 2",
+      caption: "Slide 2",
+      key: 2,
+      src: "/img/sheine-grey.jpg",
+    },
+    {
+      altText: "Slide 3",
+      caption: "Slide 3",
+      key: 3,
+      src: "/img/snake-grey.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <UncontrolledCarousel
+      items={items}
+      />
+    </div>
+  );
+};
+
+export default Carousel;
